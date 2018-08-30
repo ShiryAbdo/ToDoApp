3317603960d77a93e0daf9016b8ec0ada3ae7c28
Shiry <shaimaaenfikry@gmail.com>
2018-08-30 18:42:57 +0200

    sections

--- app/src/main/java/com/eramiexample/firstkotlinapp/activities/TaskDetalisActivity.kt
+++ app/src/main/java/com/eramiexample/firstkotlinapp/activities/TaskDetalisActivity.kt
@@ -6,8 +6,8 @@ import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
 import com.eramiexample.firstkotlinapp.R
 import com.eramiexample.firstkotlinapp.utilites.Tasks
-
 import kotlinx.android.synthetic.main.activity_task_detalis2.*
+import kotlinx.android.synthetic.main.content_task_detalis.*
 
 class TaskDetalisActivity : AppCompatActivity() {
 
@@ -17,8 +17,12 @@ class TaskDetalisActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         var task = getIntent().getExtras().get("task") as? Tasks
         Toast.makeText(this@TaskDetalisActivity, ""+task!!.id,Toast.LENGTH_LONG).show()
-
-        delet.setOnClickListener { view ->
+        taskNameupdate.isEnabled= false
+        taskNameupdate.setText(task.title)
+        task_discriptionupdate.isEnabled= false
+        task_discriptionupdate.setText(task.descriptionTask
+        )
+         delet.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
